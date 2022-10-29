b55f88a6522f05c437532abb09ed9afc5c851c1b
unknown <peacesandy04@gmail.com>
2022-10-29 03:08:30 -0700

    Added target to link tags

--- src/App.js
+++ src/App.js
@@ -37,26 +37,31 @@ function App() {
         </a>
         <a 
           href="https://training.zuri.team/" 
+          target='_blank'
           id="btn__zuri">
           Zuri Team
         </a>
         <a 
           href="http://books.zuri.team" 
+          target='_blank'
           id="books">
           Zuri Books 
         </a>
         <a 
-          href="https://books.zuri.team/python-for-beginners?ref_id=PeaceSandy" 
+          href="https://books.zuri.team/python-for-beginners?ref_id=PeaceSandy"
+          target='_blank' 
           id="book__python">
           Python Books
         </a>
         <a 
           href="https://background.zuri.team" 
+          target='_blank'
           id="pitch">
           Background Check for Coders
         </a>
         <a 
           href="https://books.zuri.team/design-rules" 
+          target='_blank'
           id="book__design">
           Design Books
         </a>

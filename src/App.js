e7ccbcb3eeacfbaf4c02fefffa737f31ef28e1c3
unknown <peacesandy04@gmail.com>
2022-10-29 03:17:09 -0700

    Added rel after each tag

--- src/App.js
+++ src/App.js
@@ -32,36 +32,42 @@ function App() {
       <section className='links-section'>
         <a href='http://twitter.com/PeaceSandy3'
         target='_blank'
+        rel='noreferrer'
         id='twitter'>
           Twitter Link
         </a>
         <a 
           href="https://training.zuri.team/" 
           target='_blank'
+          rel='noreferrer'
           id="btn__zuri">
           Zuri Team
         </a>
         <a 
           href="http://books.zuri.team" 
           target='_blank'
+          rel='noreferrer'
           id="books">
           Zuri Books 
         </a>
         <a 
           href="https://books.zuri.team/python-for-beginners?ref_id=PeaceSandy"
           target='_blank' 
+          rel='noreferrer'
           id="book__python">
           Python Books
         </a>
         <a 
           href="https://background.zuri.team" 
           target='_blank'
+          rel='noreferrer'
           id="pitch">
           Background Check for Coders
         </a>
         <a 
           href="https://books.zuri.team/design-rules" 
           target='_blank'
+          rel='noreferrer'
           id="book__design">
           Design Books
         </a>

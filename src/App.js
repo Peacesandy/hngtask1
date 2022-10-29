b3dfff46ea1c8f37e28a23e9f58ae6b2cfcccd99
unknown <peacesandy04@gmail.com>
2022-10-28 21:24:55 -0700

    completed the links section

--- src/App.js
+++ src/App.js
@@ -30,6 +30,31 @@ function App() {
         id='twitter_link'>
           Twitter Link
         </a>
+        <a 
+          href="https://training.zuri.team/" 
+          id="btn__zuri">
+          Zuri Team
+        </a>
+        <a 
+          href="http://books.zuri.team" 
+          id="books">
+          Zuri Books 
+        </a>
+        <a 
+          href="https://books.zuri.team/python-for-beginners?ref_id=PeaceSandy" 
+          id="book__python">
+          Python Books
+        </a>
+        <a 
+          href="https://background.zuri.team" 
+          id="pitch">
+          Background Check for Coders
+        </a>
+        <a 
+          href="https://books.zuri.team/design-rules" 
+          id="book__design">
+          Design Books
+        </a>
       </section>
     </main>
     </>

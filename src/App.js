b75fb2c7797ceaaf6e9cf4fa6bd1602779bda5b5
unknown <peacesandy04@gmail.com>
2022-10-28 21:10:17 -0700

    completed the profile section

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import profileimg from './assets/peace.JPG'
+import shareButton1 from './assets/share-left.png'
+import shareButton2 from './assets/share-dot.png'
 
 function App() {
   return (
@@ -10,6 +12,24 @@ function App() {
          src={profileimg}
          alt='profile img'
          />
+         <img
+         className='share-btn-desktop'
+         src={shareButton1}
+         alt='share button desktop'
+         />
+         <img
+         className='share-btn-mobile'
+         src={shareButton2}
+         alt='share button mobile'
+         />
+         <h1 id='twitter'>peacesandy3</h1>
+         <p id='slack'>Peace Sandy</p>
+      </section>
+      <section className='links-section'>
+        <a href='http://twitter.com/PeaceSandy3'
+        id='twitter_link'>
+          Twitter Link
+        </a>
       </section>
     </main>
     </>
